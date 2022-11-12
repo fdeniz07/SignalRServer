36eb99a50c66ad97d06aab515af066db91956c8d
Fatih Deniz <deniz.fatih@hotmail.de>
2022-11-13 00:06:04 +0100

    SignalR -3

--- Hubs/MyHub.cs
+++ Hubs/MyHub.cs
@@ -9,5 +9,25 @@
             //Ekstra islemler
             await Clients.All.SendAsync("receiveMessage", message);
         }
+
+        #region Overrides of Hub
+        //Sisteme bir baglanti yapildigi zaman tetiklenecek
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        #endregion
+
+        #region Overrides of Hub
+        //Sistemden bir baglanti koptugunda tetiklenecek
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        #endregion
     }
 }
